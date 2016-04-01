9a67a56c96982f9030e954d47afd1a8bf1a44efe
Aimee Matthew-John <aimeebethmj@gmail.com>
2016-04-01 14:25:51 +0100

    Imported black friday report

---
+++ cases.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <?php include ("inc/head.html");?>
+
+<style media="screen" type="text/css">
+
+.lightbox {
+    display:none;
+}
+</style>
+
+<script> 
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ 
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), 
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) 
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga'); 
+
+  ga('create', 'UA-1665647-38', 'auto'); 
+  ga('send', 'pageview'); 
+
+</script> 
+</head>
+<!-- The #page-top ID is part of the scrolling feature - the data-spy and data-target are part of the built-in Bootstrap scrollspy function -->
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+<!-- Navigation -->
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container-header">
+        <div class="navbar-header page-scroll">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1noreport-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+            <div class="logos"><a href="https://www.amnesty.org" target="_blank"><span class="ai"></span></a><a href="fa.php" target="_blank"><span class="fa"></span></a><div class="title"><a href="https://blackfriday.amnesty.org">'BLACK FRIDAY': CARNAGE IN RAFAH</a></div></div>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-ex1noreport-collapse">
+            <div class="menu-1"><h5><ul class="nav navbar-nav">
+                <li class="hidden">
+                    <a class="page-scroll" href="#page-top"></a>
+                </li>
+                <li>
+                    <a class="page-scroll menu-summary page-scroll-not" href="index.php">Summary</a>
+                </li>
+                <li>
+                    <a class="page-scroll menu-report page-scroll-not" href="report.php">Report</a>
+                </li>
+                <li>
+                    <a class="page-scroll menu-cases page-scroll-active" href="#page-top">Cases</a>
+                </li>
+            </ul>
+        </h5></div>
+</div>
+<!-- /.navbar-collapse -->
+</div>
+<!-- /.container -->
+</nav>
+
+    <?php include ("inc/cases.html");?>
+
+    <!-- Intro Section -->
+    <!-- Intro Section -->
+    <!-- Intro Section -->
+    <!-- Page --><section id="cases-1" class="top-section cases-1 section"><div class="container"><div class="row"><div class="col-lg-10">
+    <div class="box"><h1><span class="h1-heading" style="margin-bottom:16px;">
+        Cases
+    </span></h1></box>
+    <div class="img-box fit">
+        <div class="blocknav__item">
+            <a class="blocknav__box" href="#" data-ga="event,Outgoing links,Click,external" data-toggle="modal" data-target="#Hammad" style="background: url('assets/cases/hammad.jpg') center; height:100%;background-size:auto 100%;">
+                <div class="blocknav__boxtitle">BIN HAMMAD FAMILY</div>
+            </a>
+        </div>
+        <div class="blocknav__item">
+            <a class="blocknav__box" href="#" data-ga="event,Outgoing links,Click,external" data-toggle="modal" data-target="#Lafi" style="background: url('assets/cases/lafi.jpg') center; height:100%;background-size:auto 100%;">
+                <div class="blocknav__boxtitle">LAFI FAMILY</div>
+            </a>
+        </div>
+        <div class="blocknav__item">
+            <a class="blocknav__box" href="#" data-ga="event,Outgoing links,Click,external" data-toggle="modal" data-target="#Qishta" style="background: url('assets/cases/qishta.jpg') center; height:100%;background-size:auto 100%;">
+                <div class="blocknav__boxtitle">QISHTA FAMILY</div>
+            </a>
+        </div>
+        <div class="blocknav__item">
+            <a class="blocknav__box" href="#" data-ga="event,Outgoing links,Click,external" data-toggle="modal" data-target="#al-Saba" style="background: url('assets/cases/saba.jpg') center; height:100%;background-size:auto 100%;">
+                <div class="blocknav__boxtitle">al-Saba FAMILY</div>
+            </a>
+        </div>
+        <div class="blocknav__item">
+            <a class="blocknav__box" href="#" data-ga="event,Outgoing links,Click,external" data-toggle="modal" data-target="#Mohsen" style="background: url('assets/cases/mohsen.jpg') center; height:100%;background-size:auto 100%;">
+                <div class="blocknav__boxtitle">ABU MOHSEN FAMILY</div>
+            </a>
+        </div>
+        <div class="blocknav__item">
+            <a class="blocknav__box" href="#" data-ga="event,Outgoing links,Click,external" data-toggle="modal" data-target="#Duba" style="background: url('assets/cases/duba.jpg') center; height:100%;background-size:auto 100%;">
+                <div class="blocknav__boxtitle">ABU DUBA FAMILY</div>
+            </a>
+        </div>
+        <div class="blocknav__item">
+            <a class="blocknav__box" href="#" data-ga="event,Outgoing links,Click,external" data-toggle="modal" data-target="#al-Gharib" style="background: url('assets/cases/gharib.jpg') center; height:100%;background-size:auto 100%;">
+                <div class="blocknav__boxtitle">AL-GHARIB FAMILY</div>
+            </a>
+        </div>
+        <div class="blocknav__item">
+            <a class="blocknav__box" href="#" data-ga="event,Outgoing links,Click,external" data-toggle="modal" data-target="#Arafat" style="background: url('assets/cases/arafat.jpg') center; height:100%;background-size:auto 100%;">
+                <div class="blocknav__boxtitle">ARAFAT FAMILY</div>
+            </a>
+        </div>
+        <div class="blocknav__item">
+            <a class="blocknav__box" href="#" data-ga="event,Outgoing links,Click,external" data-toggle="modal" data-target="#Hospital" style="background: url('assets/cases/hospital.jpg') center; height:100%;background-size:auto 100%;">
+                <div style="margin-top:97.5px;"class="blocknav__boxtitle">ABU YOUSSEF AL-NAJJAR HOSPITAL</div>
+            </a>
+        </div>
+        <div class="blocknav__item">
+            <a class="blocknav__box" href="#" data-ga="event,Outgoing links,Click,external" data-toggle="modal" data-target="#Musabbeh" style="background: url('assets/cases/musabbeh.jpg') center; height:100%;background-size:auto 100%;">
+                <div style="margin-top:94.25px;" class="blocknav__boxtitle">AMBULANCE IN MUSABBEH, EASTERN RAFAH</div>
+            </a>
+        </div>
+        <div class="blocknav__item">
+            <a class="blocknav__box" href="#" data-ga="event,Outgoing links,Click,external" data-toggle="modal" data-target="#Wahhab" style="background: url('assets/cases/wahhab.jpg') center; height:100%;background-size:auto 100%;">
+                <div class="blocknav__boxtitle">ABDEL-WAHHAB FAMILY</div>
+            </a>
+        </div>
+        <div class="blocknav__item">
+            <a class="blocknav__box" href="#" data-ga="event,Outgoing links,Click,external" data-toggle="modal" data-target="#Taha" style="background: url('assets/cases/taha.jpg') center; height:100%;background-size:auto 100%;">
+                <div class="blocknav__boxtitle">ABU TAHA FAMILY</div>
+            </a>
+        </div>
+        <div class="blocknav__item">
+            <a class="blocknav__box" href="#" data-ga="event,Outgoing links,Click,external" data-toggle="modal" data-target="#Zoroub" style="background: url('assets/cases/zoroub.jpg') center; height:100%;background-size:auto 100%;">
+                <div class="blocknav__boxtitle">ZOROUB FAMILY</div>
+            </a>
+        </div>
+        <div class="blocknav__item">
+            <a class="blocknav__box" href="#" data-ga="event,Outgoing links,Click,external" data-toggle="modal" data-target="#Ghoul" style="background: url('ghoul') center; height:100%;background-size:auto 100%;">
+                <div class="blocknav__boxtitle">NEIREB, GHOUL, MANYARAWI, ABU AYTA FAMILIES</div>
+            </a>
+        </div>
+        <div class="blocknav__item">
+            <a class="blocknav__box" href="#" data-ga="event,Outgoing links,Click,external" data-toggle="modal" data-target="#Suleiman" style="background: url('assets/cases/suleiman.jpg') center; height:100%;background-size:auto 100%;">
+                <div class="blocknav__boxtitle">ABU SULEIMAN FAMILY</div>
+            </a>
+        </div>
+    </div>
+</div>
+<div style="height:5rem;"></div>   
+
+    <script src="js/less.min.js" type="text/javascript"></script>
+    <script src="js/jquery.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Scrolling Nav JavaScript -->
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/scrolling-nav.js"></script>
+    <!-- Parallax JavaScript -->
+    <script src="js/parallax.js"></script>
+    <script src="js/bootstrap-magnify.min.js"></script>
+    <script src="js/lightbox.js"></script>
+
+<script>
+$('[data-toggle="popover"]').popover({
+    container: 'body'
+});</script>
+<script>
+    $(function () {
+  $('[data-toggle="popover"]').popover()
+})</script>
+
+
+<script type="text/javascript">
+setTimeout(function(){var a=document.createElement("script");
+var b=document.getElementsByTagName("script")[0];
+a.src=document.location.protocol+"//script.crazyegg.com/pages/scripts/0032/7104.js?"+Math.floor(new Date().getTime()/3600000);
+a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
+</script>
+</body>
+
+</html>
---
+++ inc/footnotes.php
@@ -0,0 +1,12 @@
+<?php
+
+function footnote001() { ?>
+<sup><a data-placement="top" data-toggle="popover" tabindex="0" role="button" data-content="
+	footnote1" data-html="true"><span class="glyphicon glyphicon-bookmark"></span></a></sup>
+<?php }
+
+function footnote002() { ?>
+<sup><a data-placement="top" data-toggle="popover" tabindex="0" role="button" data-content="<a href='http://www.youtube.com'>footnote2</a>" target="_blank_"data-html="true"><span class="glyphicon glyphicon-bookmark"></span></a></sup>
+<?php }
+
+?>
