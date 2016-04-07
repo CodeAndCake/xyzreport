1f1203d9f28fbb62e34d6ecb71d1069297e32e25
Matteo Menapace <hello@baddeo.com>
2016-04-07 19:50:42 +0100

    Local config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,90 +1,90 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		shell: {
-			jekyllBuild: {
-				command: 'jekyll build --config _config.local.yml'
-			},
-			jekyllServe: {
-				command: "jekyll serve --baseurl ''"
-			}
-		},
-		connect: {
-			server: {
-				options: {
-					port: 8080,
-					base: '_site'
-				}
-			}
-		},
-		sass: {
-			dist: {
-				options: {
-					style: 'compressed'
-				},
-				files: {
-					'sassy.css': '_sass/sassy.scss'
-				}
-			}
-		},
-		watch: {
-			scripts: {
-				files: [
-					'_config.yml',
-					'index.html',  
-					'_layouts/**',
-					'_posts/**',
-					'_includes/**'
-				],
-				tasks: ['shell:jekyllBuild'],
-				options: {
-					livereload: true
-				},
-			},
-			css: {
-				files: [
-					'_sass/**'
-				],
-				tasks: ['sass', 'shell:jekyllBuild'],
-				options: {
-					livereload: true
-				},
-			},
-		},
-		browserSync: {
-			default_options: {
-				bsFiles: {
-					src: [
-						'./_site/*.html',
-						'./_site/*.css'
-					]
-				},
-				options: {
-					watchTask: true,
-					server: './_site',
-					watchOptions: {
-						ignoreInitial: true,
-						ignored: ['*.txt', 'node_modules', '.git'],
-					},
-					browser: 'google chrome',
-					notify: false
-				}	
-			}
-		},
-		jekyll: {                             
-    		options: {                        
-    			raw: 'baseurl: ""'
-    		}
-    	}	
-	})
+  grunt.initConfig({
+    shell: {
+      jekyllBuild: {
+        command: 'jekyll build --config _config.yml,_config.local.yml'
+      },
+      jekyllServe: {
+        command: "jekyll serve --baseurl ''"
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8080,
+          base: '_site'
+        }
+      }
+    },
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'sassy.css': '_sass/sassy.scss'
+        }
+      }
+    },
+    watch: {
+      scripts: {
+        files: [
+          '_config.yml',
+          'index.html',  
+          '_layouts/**',
+          '_posts/**',
+          '_includes/**'
+        ],
+        tasks: ['shell:jekyllBuild'],
+        options: {
+          livereload: true
+        },
+      },
+      css: {
+        files: [
+          '_sass/**'
+        ],
+        tasks: ['sass', 'shell:jekyllBuild'],
+        options: {
+          livereload: true
+        },
+      },
+    },
+    browserSync: {
+      default_options: {
+        bsFiles: {
+          src: [
+            './_site/*.html',
+            './_site/*.css'
+          ]
+        },
+        options: {
+          watchTask: true,
+          server: './_site',
+          watchOptions: {
+            ignoreInitial: true,
+            ignored: ['*.txt', 'node_modules', '.git'],
+          },
+          browser: 'google chrome',
+          notify: false
+        } 
+      }
+    },
+    jekyll: {
+      options: {                        
+        raw: 'baseurl: ""'
+      }
+    } 
+  })
 
-	grunt.loadNpmTasks('grunt-contrib-sass')
-	grunt.loadNpmTasks('grunt-contrib-connect')
-	grunt.loadNpmTasks('grunt-contrib-watch')
-	grunt.loadNpmTasks('grunt-shell')
-	grunt.loadNpmTasks('grunt-browser-sync');
-	grunt.loadNpmTasks('grunt-jekyll');
-	
-	grunt.registerTask('tasting', ['browserSync', 'watch'])
-	grunt.registerTask('default', ['shell:jekyllBuild', 'watch'])
-	grunt.registerTask('yolo', ['jekyll', 'watch'])
+  grunt.loadNpmTasks('grunt-contrib-sass')
+  grunt.loadNpmTasks('grunt-contrib-connect')
+  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-shell')
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-jekyll');
+  
+  grunt.registerTask('tasting', ['browserSync', 'watch'])
+  grunt.registerTask('default', ['shell:jekyllBuild', 'watch'])
+  // grunt.registerTask('yolo', ['jekyll', 'watch'])
 }
