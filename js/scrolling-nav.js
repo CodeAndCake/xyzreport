56d01b93341801e15198c68a5ebff257ccb6c8df
Matteo Menapace <hello@baddeo.com>
2016-04-08 13:22:39 +0100

    Scrolling listener

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,39 +1,45 @@
 //jQuery to collapse the navbar on scroll
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
+
+var offset = 50,
+    $navbarFixedTop = $(".navbar-fixed-top"),
+    $navbarNav = $(".navbar-nav"),
+    $menu2 = $(".menu-2"),
+    $navbarEx1Collapse = $(".navbar-ex1-collapse"),
+    $title = $(".title"),
+    $pageNav = $(".page-nav")
+
+$(window).scroll(function() 
+{
+  if ($(".navbar").offset().top > offset) 
+  {
+    $navbarFixedTop.addClass("top-nav-collapse");
+    $navbarNav.addClass("navbar-nav-collapse");
+    $menu2.addClass("menu-2-collapse");
+    $navbarEx1Collapse.addClass("ex1-collapse");
+    $title.addClass("title-collapse");
+    $pageNav.addClass("page-nav-collapse");
+  } 
+  else 
+  {
+    $navbarFixedTop.removeClass("top-nav-collapse");
+    $navbarNav.removeClass("navbar-nav-collapse");
+    $menu2.removeClass("menu-2-collapse");
+    $navbarEx1Collapse.removeClass("ex1-collapse");
+    $title.removeClass("title-collapse");
+    $pageNav.removeClass("page-nav-collapse");
+  }
 });
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-nav").addClass("navbar-nav-collapse");
-    } else {
-        $(".navbar-nav").removeClass("navbar-nav-collapse");
-    }
-});
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".menu-2").addClass("menu-2-collapse");
-    } else {
-        $(".menu-2").removeClass("menu-2-collapse");
-    }
-});
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-ex1-collapse").addClass("ex1-collapse");
-    } else {
-        $(".navbar-ex1-collapse").removeClass("ex1-collapse");
-    }
-});
+
 //jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+$(function() 
+{
+  $('a.page-scroll').bind('click', function(event) 
+  {
+    var $anchor = $(this);
+    $('html, body').stop().animate(
+    {
+      scrollTop: $($anchor.attr('href')).offset().top
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+  });
 });
